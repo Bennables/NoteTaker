e02ddfd43b301d7ffa4b24ef0dfb9965aab71eb0
Ben Sirivallop <bensirivallop@Bens-M3-Air-5.local>
2025-06-14 16:07:59 -0700

    created endpoints for notes REST

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,24 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+    //get
+    // res.send("HEHE");
+    res.status(200).send("You have 5 notes");
+})
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).send("New Note!");
+})
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(202).send("Updated successfully");
+})
+
+app.delete("/api/nots/:id"), (req, res) => {
+    res.status(203).send("Successfully deleted")
+}
+
 app.listen(5001, () =>{
     console.log("Started connection port: 5001");
 })
