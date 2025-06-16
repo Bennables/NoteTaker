6476e220344bc1d0d892618eff0e46c04985e383
Ben Sirivallop <bensirivallop@Bens-M3-Air-6.local>
2025-06-16 11:38:17 -0700

    handling for other ids

--- backend/src/controller/noteControllers.js
+++ backend/src/controller/noteControllers.js
@@ -24,20 +24,22 @@ export const postNote = async (req, res) => {
         res.status(201).json({message: "Note saved successfully", savedNote});
 
     } catch (error) {
-        console.error("Could not save new note", error);
+        console.log("Could not save new note", error);
         res.status(500).json({message: "Couldn't save the new note"});
     }
 }
 
 export const putNote = async (req, res) => { 
     try {
-        res.status(202).send("Updated the note");
         const {title, content} = req.body;
-        await Note.findByIdAndUpdate(req.params.id, {title, content});
-        res.status(200).json({message: "note updated successfully"})
+        const updatedNote = await Note.findByIdAndUpdate(req.params.id, {title, content});
+        if(!updatedNote){
+            return res.status(404).json({message: "Note not found"});
+        }
+        res.status(200).json({message: "note updated successfully"});
 
-    } catch {
-        console.error("Could not update note", error);
+    } catch (error){
+        console.log("Could not update note", error);
         res.status(500).json({message: "Couldn't update note"});
     }
 }
