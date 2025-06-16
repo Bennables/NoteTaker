63ec4f787336310f85f6e417d11368a5d3563e8a
Ben Sirivallop <bensirivallop@Bens-M3-Air-6.local>
2025-06-16 11:28:19 -0700

    can update notes now

--- backend/src/controller/noteControllers.js
+++ backend/src/controller/noteControllers.js
@@ -19,8 +19,9 @@ export const postNote = async (req, res) => {
         const newNote = new Note({title: title, content: content});
 
         //save is from the mongoose schema
-        await newNote.save(); // this waits for confirmation from the newnote
-        res.status(201).json({message: "Note saved successfully"});
+        const savedNote = await newNote.save(); // this waits for confirmation from the newnote
+
+        res.status(201).json({message: "Note saved successfully", savedNote});
 
     } catch (error) {
         console.error("Could not save new note", error);
@@ -28,8 +29,17 @@ export const postNote = async (req, res) => {
     }
 }
 
-export const putNote = (req, res) => { 
-    res.status(202).send("Updated the note");
+export const putNote = async (req, res) => { 
+    try {
+        res.status(202).send("Updated the note");
+        const {title, content} = req.body;
+        await Note.findByIdAndUpdate(req.params.id, {title, content});
+        res.status(200).json({message: "note updated successfully"})
+
+    } catch {
+        console.error("Could not update note", error);
+        res.status(500).json({message: "Couldn't update note"});
+    }
 }
 
 export const deleteNote = (req, res) => {
