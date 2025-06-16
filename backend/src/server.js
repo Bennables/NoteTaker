8888487d80bb784c2b0735f035c1ae9db3cc3d5b
Ben Sirivallop <bensirivallop@Bens-M3-Air-7.local>
2025-06-16 14:17:33 -0700

    we now have rate limiting(optionally"

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import noteRoutes from "./routes/noteRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
@@ -10,7 +10,26 @@ const PORT = process.env.PORT || 5001;
 const app = express();
 console.log(process.env.CONNECTION)
 
+
+//allows use to get req.body
 app.use(express.json());
+
+//optional middlear, more useful if we had auth
+// app.use(rateLimiter);
+
+//middleware: often used for auth on server side
+app.use((req, res, next) =>{
+    //the way that I understand this.
+    // we call the func in nextRoutes, req and res is the same, but now we add
+    // next, which is         v this v
+    //router.delete("/:id" , deleteNote);
+    
+    //it's processing after the get the req but before we call any functions.
+    console.log("We got a request");
+    console.log(`The method is ${req.method} and the url is ${req.url}`);
+    next();
+})
+
 app.use("/api/notes", noteRoutes);
 
 connectDB();
