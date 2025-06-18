4144e925836f2ac4495ee6c25ae61c4ef37ce290
Ben Sirivallop <bensirivallop@Bens-M3-Air-8.local>
2025-06-18 12:32:30 -0700

    got some success/fail icons in there

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,25 @@ import HomePage from './Pages/HomePage'
 import { Routes, Route } from 'react-router'
 import NoteDetailPage from './Pages/NoteDetailPage'
 import CreatePage from "./Pages/CreatePage"
+import {toast} from 'react-hot-toast';
+
 
 const App = () => {
   return (
     <div>
-      <p> THIS IS THE MAIN PAGE </p>
+     
+      <p> THIS IS THE MAIN PAGEasdfasdf </p>
       {/* this part creates the links to other pages */}
-      
+      {/* {()=>toast.success("JFDLFKSJLDFJDLK")} */}
+      <button onClick={() => toast.success("congrats")}>CLICK HERERRRR</button>
+      <button onClick={() => toast.error("congrats")}>CLICK HERERRRR</button>
+    
       <Routes>
         <Route path = "/" element={<HomePage />}> </Route>
         <Route path = '/create' element = {<CreatePage />}></Route>
         <Route path = "/note/:id" element = {<NoteDetailPage />}></Route>
       </Routes>
+      
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// just learned this. importing in the curly brackets gets specifically what you're importing
+// if there's only one thing in module, then you can just import
+// otherwise it will get confused.
+import {Toaster} from "react-hot-toast"
 
 import {BrowserRouter} from 'react-router';
 
@@ -9,6 +13,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster/>
     </BrowserRouter>
     
   </StrictMode>,
