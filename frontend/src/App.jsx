13938d841afc0872f3249b5b600eb1ae8cdedc6e
Ben Sirivallop <bensirivallop@Bens-M3-Air-8.local>
2025-06-18 12:05:08 -0700

    starting pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 //barebones gotten from "rafce" + tab
 import React from 'react'
+import HomePage from './Pages/HomePage'
+import { Routes, Route } from 'react-router'
+import NoteDetailPage from './Pages/NoteDetailPage'
+import CreatePage from "./Pages/CreatePage"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <p> THIS IS THE MAIN PAGE </p>
+      {/* this part creates the links to other pages */}
+      
+      <Routes>
+        <Route path = "/" element={<HomePage />}> </Route>
+        <Route path = '/create' element = {<CreatePage />}></Route>
+        <Route path = "/note/:id" element = {<NoteDetailPage />}></Route>
+      </Routes>
+    </div>
   )
 }
 
