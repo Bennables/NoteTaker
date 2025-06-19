b473070666e4b2514bdef848cce961ddda83b82e
Ben Sirivallop <bensirivallop@Bens-M3-Air-8.local>
2025-06-18 20:09:59 -0700

    wrapping up the day!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,18 @@ import NoteDetailPage from './Pages/NoteDetailPage'
 import CreatePage from "./Pages/CreatePage"
 import {toast} from 'react-hot-toast';
 import "./input.css"
+import NavBar from './Components/NavBar'
+import RateLimitedUI from './Components/RateLimited'
+import NoteCard from './Components/NoteCard'
 
 
 const App = () => {
   return (
     <div data-theme = "forest">
-      
-      <p> THIS IS THE MAIN PAGEasdfasdf </p>
       {/* this part creates the links to other pages */}
-      {/* {()=>toast.success("JFDLFKSJLDFJDLK")} */}
       <button onClick={() => toast.success("congrats")} className="btn">CLICK HERERRRR</button>
       <button onClick={() => toast.error("congrats")}>CLICK HERERRRR</button>
-      <button class="btn btn-primary">One</button>
-      <button class="btn btn-secondary">Two</button>
-      <button class="btn btn-accent btn-outline">Three</button>
-      <p className="btn">TAILWIND TEST</p>
+      {/* <RateLimitedUI></RateLimitedUI> */}
       <Routes>
         <Route path = "/" element={<HomePage />}> </Route>
         <Route path = '/create' element = {<CreatePage />}></Route>
