e2e083c3044ea6d2a7c5d3fb13f3c604f171902a
Ben Sirivallop <bensirivallop@Bens-M3-Air-55.local>
2025-06-19 18:33:12 -0700

    if theres no notes, then no note found

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -6,6 +6,7 @@ import {toast} from "react-hot-toast"
 import RateLimitedUI from '../Components/RateLimited';
 import NoteCard from '../Components/NoteCard';
 import api from "../lib/axios"
+import NotesNotFound from '../Components/NotesNotFound';
 
 const HomePage = () => {
 
@@ -54,6 +55,8 @@ const HomePage = () => {
         }else{
           toast.error("failed to load data");
         }
+      } finally{
+        setLoading(false);
       }
     };
   fetchNotes();
@@ -69,6 +72,10 @@ const HomePage = () => {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className = "text-center text-primary py-10">Loading Notes</div>}
+
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+
+
         {notes.length > 0 && !isRateLimited && (
           <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
             {notes.map(note => (
